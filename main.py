0b943417616202c4452a7590b65d7f6bd7dd932c
Saif Ayman Fathi Mohamed Metwally <65094581+metwally-saif@users.noreply.github.com>
2023-11-20 19:11:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from statement import Statement
 def main() -> None:
     statement_calculator = Statement()
     result = statement_calculator.statement(
-        json.load(open('./_chapter_1/invoices.json'))[0],
-        json.load(open('./_chapter_1/plays.json')),
+        json.load(open('invoices.json'))[0],
+        json.load(open('plays.json')),
     )
     print(result)
 
